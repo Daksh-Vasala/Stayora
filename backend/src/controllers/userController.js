90ea7bc91822b0fcef8f712c9e004708eea21157
Daksh Vasala <vasaladaksh5@gmail.com>
2026-03-09 12:29:12 +0530

    installed bcrypt and added functionality to save hashedPassword in db

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel.js");
+const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
   try {
@@ -12,20 +13,27 @@ const register = async (req, res) => {
       });
     }
 
+    // hash password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(userData.password, saltRounds);
+
+    userData.password = hashedPassword;
+
     const createdUser = await User.create(userData);
 
+    // hide password in response
     createdUser.password = undefined;
 
     res.status(201).json({
       message: "User created successfully",
-      data: createdUser
+      data: createdUser,
     });
 
   } catch (error) {
     console.log(error);
     res.status(500).json({
       message: "Internal server error",
-      error
+      error,
     });
   }
 };
