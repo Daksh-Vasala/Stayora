ba338dce8ac3247125bcb1d63ea52bbe3af2dc9a
Daksh Vasala <162029101+Daksh-Vasala@users.noreply.github.com>
2026-06-02 13:55:42 +0530

    Change axios base URL to production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { ToastContainer } from "react-toastify";
 import AppRouter from "./router/AppRouter";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000"
+axios.defaults.baseURL = "https://stayora-backend-x75x.onrender.com"
 axios.defaults.withCredentials = true
 
 function App() {
